8ca9f2f17014d5f33b2a54ef11d96da12bf1e10f
cristina1084 <cristina.johns1084@gmail.com>
2019-07-31 13:58:01 +0530

    display

--- html/js/library.js
+++ html/js/library.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){             //jQuery
     $("#load").hide();
-    $("#tp1").click(function(){
+    $("#tp1,#cat").click(function(){
+        var cat = $("#cat").val();
         $.ajax({                        //AJAX format
             type:"GET",                 //type mentions the method to be used 
             url :"librarydata.json",         //can provide API link
@@ -10,14 +11,18 @@ $(document).ready(function(){             //jQuery
             success : function(data){
                 $("#load").hide();
                 console.log(data);
+                console.log(cat);
                 
                 var card="<div class='card-deck'>";
                 for (var j in data){
-                    card+="<div class='col-4 col-sm-4 col-md-4'> <div class='card'> <img class='card-img-top' src=" + data[j].urlToImage + ">";
-                    card+="<div class='card-body'> <h5 class='card-title'>"+ data[j].bookTitle +"</h5>";
-                    card+="<h6 class='card-subtitle mb-2 text-muted'>" + data[j].author +"</h6> ";
-                    card+="<p class='card-text text-justify'>"+ data[j].description + "</p> </div>";
-                    card+="<div class='card-footer'>"+ data[j].price+"</div></div></div> <br>";
+                    if (cat == data[j].genre)
+                    {
+                        card+="<div class='col-4 col-sm-4 col-md-4'> <div class='card'> <img class='card-img-top' src=" + data[j].urlToImage + ">";
+                        card+="<div class='card-body'> <h5 class='card-title'>"+ data[j].bookTitle +"</h5>";
+                        card+="<h6 class='card-subtitle mb-2 text-muted'>" + data[j].author +"</h6> ";
+                        card+="<p class='card-text text-justify'>"+ data[j].description + "</p> </div>";
+                        card+="<div class='card-footer'>"+ data[j].price+"</div></div></div> <br>";
+                    }
                 }
                 card+="</div>";
                 $(".results").html(card);
